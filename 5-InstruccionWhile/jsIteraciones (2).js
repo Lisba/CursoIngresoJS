ddd09054b34ff34bfab2b5e7f4250f27d7b919e4
Lisbaldy <43254199+Lisba@users.noreply.github.com>
2019-07-19 20:59:20 -0300

    Ejercicios 1 al 6 de while

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,15 @@
 function mostrar()
 {
-	alert('iteración while');
+	
+	var contador = 11;
+
+	while (contador > 1)
+	{
+
+		contador -= 1;
+		console.log(contador);
+
+	}
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+    var clave = prompt("ingrese el número clave.");
+
+    while (clave != "utn750")
+    {
+
+        clave = prompt("INCORRECTO, ingrese clave nuevamente");
+
+    }
 
 
 }//FIN DE LA FUNCIÓN
