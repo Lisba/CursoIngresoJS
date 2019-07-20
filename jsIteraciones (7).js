ef51cdc33e226168d3f1bfdea23d5e6162ef9fb6
Lisbaldy <43254199+Lisba@users.noreply.github.com>
2019-07-19 21:58:02 -0300

    add concepto explicado

---
+++ jsIteraciones (7).js
@@ -0,0 +1,26 @@
+function mostrar()
+{
+
+// 	var contador=0;
+// 	var acumulador=0;
+// 	var respuesta='si';
+
+
+// document.getElementById('suma').value=acumulador;
+// document.getElementById('promedio').value=acumulador/contador;
+
+// var numero;
+// var numeroDos;
+// var suma;
+
+// numeroDos = 33;//false
+// numeroDos = "33";//false
+// numeroDos = "lalala"//true
+// suma = isNaN(numeroDos);
+
+// if(isNaN(numero) || numero > 10)
+// {
+
+// }
+
+}//FIN DE LA FUNCIÓN
