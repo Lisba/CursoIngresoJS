12c9fe90572dcf45e025128d3af9e0be16d09123
Lisbaldy <lisba7474@gmail.com>
2019-07-21 14:44:55 -0300

    actiualización de apuntes

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
+    // var numero;
+
+    // numero = prompt("Ingresa un número!");
+
+    // while( isNaN(numero) )
+    // {
+    //     numero = prompt("EROOOR, intenta ingresar un número de nuevo!");
+    // }
+
+    // alert("Bien!! Ingresaste un número");
 
 }
