1563903e9a36a83a30a74a88fac082c9bccc5299
Lisbaldy <43254199+Lisba@users.noreply.github.com>
2019-07-31 21:54:19 -0300

    Adelanto del 9 del parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,34 @@
 function mostrar()
 {
 
+    var marca;
+    var peso;
+    var temperatura;
+    var respuesta;
+
+    do
+    {
+        
+        marca = prompt("Ingrese la marca del producto:");
+
+        peso = prompt("Ingrese el peso del producto:");
+        peso = parseInt(peso);
+
+        while(isNaN(peso) || peso < 1 || peso > 100)
+        {
+            peso = prompt("ERROR, Ingrese el peso nuevamente entre 1 y 100 kgs:");
+        }
+
+        temperatura = prompt("Ingrese la temperatura de almacenamiento del producto en ºC:");
+        temperatura = parseInt(temperatura);
+
+        while(isNaN(temperatura) || temperatura < -30 || temperatura > 30)
+        {
+            temperatura = prompt("Ingrese nuevamente la temperatura de almacenamiento del producto entre -30 y 30 en ºC:");
+        }
+
+        respuesta = confirm("Desea seguir agregando productos?");
+
+    } while(respuesta)
+
 }
